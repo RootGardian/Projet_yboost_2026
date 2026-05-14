9f4e2b90737411179e6d8e1adc63a89b7278170f
SatanVI <Ahmedbangoura852@gmail.com>
2026-05-14 23:58:37 +0100

    MAJ MFA & Gemme 4

--- ClinicFlow/ClinicFlow/backend/src/routes/consultationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getConsultationDetails } = require('../controllers/consultationController');
-const { protect } = require('../middlewares/authMiddleware');
-const auditLog = require('../middlewares/auditMiddleware');
-
-router.get('/:appointmentId', protect, auditLog('READ_CONSULTATION_DETAILS'), getConsultationDetails);
-
-module.exports = router;
